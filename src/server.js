c5407ef4c7ff1aa9118ec6e7c53c50b0422c9c4b
VVIN <binibin11@naver.com>
2021-04-07 18:31:55 +0900

    #15.2 -210407

--- src/server.js
+++ src/server.js
@@ -4,6 +4,10 @@ const PORT = 4400;
 
 const app = express();
 
+const handleHome = () => console.log("Somebody is trying to go home.");
+
+app.get("/", handleHome);
+
 const handleListening = () =>
   console.log(`🐾Server listening on port http://localhost:${PORT} 🎈`);
 
