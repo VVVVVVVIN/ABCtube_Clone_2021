9ed677b23b867b4f9527e64a09d61b46149143e5
VVIN <binibin11@naver.com>
2021-04-08 20:41:18 +0900

    #15.11 - 210408

--- src/server.js
+++ src/server.js
@@ -1,19 +1,24 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4400;
 
 const app = express();
+const logger = morgan("dev");
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
+const home = (req, res) => {
+  console.log("Home🎶");
+  return res.send("hello");
 };
 
-const handleHome = (req, res) => {
-  return res.send("I love middlewares");
+const login = (req, res) => {
+  console.log("login⚽");
+  return res.send("login");
 };
 
-app.get("/", logger, handleHome);
+app.use(logger);
+app.get("/", home);
+app.get("/login", login);
 
 const handleListening = () =>
   console.log(`🐾Server listening on port http://localhost:${PORT} 🎈`);
