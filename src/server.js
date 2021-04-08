401a17ff7c8030783216ddda1249dfafa30bd1f4
VVIN <binibin11@naver.com>
2021-04-08 17:26:29 +0900

    #15.5 - 210408

--- src/server.js
+++ src/server.js
@@ -4,16 +4,16 @@ const PORT = 4400;
 
 const app = express();
 
+const gossipMiddleware = (req, res, next) => {
+  console.log(`Someone is going to: ${req.url}`);
+  next();
+};
+
 const handleHome = (req, res) => {
-  return res.end();
+  return res.send("I love middlewares");
 };
 
-const handleLogin = (req, res) => {
-  return res.send("Hello! login");
-};
-
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.get("/", gossipMiddleware, handleHome);
 
 const handleListening = () =>
   console.log(`🐾Server listening on port http://localhost:${PORT} 🎈`);
