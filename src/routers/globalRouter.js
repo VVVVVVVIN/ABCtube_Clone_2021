963352a591670c9133e11f3e834f859bf647895d
VVIN <binibin11@naver.com>
2021-04-11 09:58:13 +0900

    #16.2 - 2120411

---
+++ src/routers/globalRouter.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const globalRouter = express.Router();
+
+const handleHome = (req, res) => res.send("Home🥇");
+
+globalRouter.get("/", handleHome);
+
+export default globalRouter;
