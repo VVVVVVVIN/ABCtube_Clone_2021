778cbe2ff8c9e03e2fe19202a1e365949ca8c245
VVIN <binibin11@naver.com>
2021-04-12 17:59:37 +0900

    #17.9 - 210412

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,12 +2,27 @@ export const trending = (req, res) => {
   const videos = [
     {
       title: "비디오",
+      rating: 1,
+      comments: 2,
+      createdAt: "7 minutes ago",
+      views: 11,
+      id: 1,
     },
     {
       title: "삐디오",
+      rating: 2,
+      comments: 1,
+      createdAt: "8 minutes ago",
+      views: 11,
+      id: 1,
     },
     {
       title: "비띠오",
+      rating: 3,
+      comments: 0,
+      createdAt: "9 minutes ago",
+      views: 11,
+      id: 1,
     },
   ];
   return res.render("home", { pageTitle: "Home", videos });
