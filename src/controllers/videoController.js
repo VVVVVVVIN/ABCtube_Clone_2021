89057de4561b01b44f9129b9e394be16b82fb70e
VVVVVVVIN <77380046+VVVVVVVIN@users.noreply.github.com>
2021-04-14 17:21:12 +0900

    #18.0 - 210414

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,33 +1,37 @@
+let videos = [
+  {
+    title: "비디오",
+    rating: 1,
+    comments: 2,
+    createdAt: "7 minutes ago",
+    views: 11,
+    id: 1,
+  },
+  {
+    title: "삐디오",
+    rating: 2,
+    comments: 1,
+    createdAt: "8 minutes ago",
+    views: 11,
+    id: 2,
+  },
+  {
+    title: "비띠오",
+    rating: 3,
+    comments: 0,
+    createdAt: "9 minutes ago",
+    views: 11,
+    id: 3,
+  },
+];
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "비디오",
-      rating: 1,
-      comments: 2,
-      createdAt: "7 minutes ago",
-      views: 11,
-      id: 1,
-    },
-    {
-      title: "삐디오",
-      rating: 2,
-      comments: 1,
-      createdAt: "8 minutes ago",
-      views: 11,
-      id: 1,
-    },
-    {
-      title: "비띠오",
-      rating: 3,
-      comments: 0,
-      createdAt: "9 minutes ago",
-      views: 11,
-      id: 1,
-    },
-  ];
   return res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => res.render("watch");
+export const see = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching ${video.title}` });
+};
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
