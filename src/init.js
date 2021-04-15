215ae63cb82c26ef5d28945e8ea92e475ba0561c
VVVVVVVIN <77380046+VVVVVVVIN@users.noreply.github.com>
2021-04-15 16:41:42 +0900

    #18.15 - 210415

--- src/init.js
+++ src/init.js
@@ -5,6 +5,6 @@ import app from "./server";
 const PORT = 4400;
 
 const handleListening = () =>
-  console.log(`🐾Server listening on port http://localhost:${PORT} 🎈`);
+  console.log(`🐾 Server listening on port http://localhost:${PORT} 🎈`);
 
 app.listen(PORT, handleListening);
