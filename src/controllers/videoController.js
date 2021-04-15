61ff9c951ba66ac0446b94a53ef80f8a3d56ed74
VVVVVVVIN <77380046+VVVVVVVIN@users.noreply.github.com>
2021-04-15 17:24:50 +0900

    #18.16 - 210415

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,6 +2,7 @@ import Video from "../models/Video";
 
 export const home = async (req, res) => {
   const videos = await Video.find({});
+  console.log(videos);
   return res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = (req, res) => {
@@ -24,15 +25,17 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
 
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
-  const video = new Video({
+  await Video.create({
     title,
     description,
     createdAt: Date.now(),
     hashtags: hashtags.split(",").map((word) => `#${word}`),
-    meta: { views: 0, rating: 0 },
+    meta: {
+      views: 0,
+      rating: 0,
+    },
   });
-  console.log(video);
   return res.redirect("/");
 };
